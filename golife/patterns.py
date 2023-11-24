f787d51a6a8e6f0affc5cd0e32a0052377f69eff
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2023-11-24 07:13:26 +0300

    test(patterns): load patterns

--- golife/patterns.py
+++ golife/patterns.py
@@ -3,6 +3,16 @@ Used for Patterns that will hold the name of a pattern and the co-ordinates of a
 """
 from typing import Set, Tuple, List, Dict
 from dataclasses import dataclass
+from pathlib import Path
+
+# If using a Python version lower than 3.11, then the import will fail and try to import from the tomli library.
+# Which will need to be installed
+try:
+    import tomllib
+except ImportError:
+    import tomli as tomllib  # type: ignore[no-redef]
+
+PATTERNS_FILE = Path(__file__).parent / "patterns.toml"
 
 
 @dataclass
@@ -34,3 +44,32 @@ class Pattern:
         return cls(
             name=name, alive_cells={tuple(cell) for cell in toml_data["alive_cells"]}
         )
+
+
+def get_pattern(name: str, filename: Path = PATTERNS_FILE) -> Pattern:
+    """
+    Retrieves a single pattern from the given file and creates a Pattern
+
+    Args:
+          name (str): name of the pattern to load from file.
+          filename (Path): path to a pattern file to load
+    Return:
+        Pattern: instance of a pattern
+    """
+    data = tomllib.loads(filename.read_text(encoding="utf-8"))
+    return Pattern.from_toml(name, toml_data=data[name])
+
+
+def get_all_patterns(filename: Path = PATTERNS_FILE) -> List[Pattern]:
+    """
+    Retrieves all patterns from the given file and creates Patterns
+
+    Args:
+          filename (Path): path to a pattern file to load
+    Return:
+        List: list of pattern instances.
+    """
+    data = tomllib.loads(filename.read_text(encoding="utf-8"))
+    return [
+        Pattern.from_toml(name, toml_data=toml_data) for name, toml_data in data.items()
+    ]
